a310ee8adfa270294273e6bebc68dd8f9877a5c9
LuvayBharmal <bharmalluvay@gmail.com>
2021-12-28 19:29:58 +0530

    First Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8-v!b&4n^hi4*cz1k-168va!0wk(vft+m87v#%-2#(pu7hl!xx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG =  False
 
 ALLOWED_HOSTS = ['*']
 
